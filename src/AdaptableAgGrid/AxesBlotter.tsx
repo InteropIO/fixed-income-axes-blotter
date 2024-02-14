cf7a314f42516b6d95938d54e021488e3610a8b2
Bogdan Dinchev <bogdan.dinchev@interop.io>
2024-02-14 14:43:05 +0200

    subscribe for changes through WS api

--- src/AdaptableAgGrid/AxesBlotter.tsx
+++ src/AdaptableAgGrid/AxesBlotter.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { Root } from 'react-dom/client';
 import { GridOptions } from '@ag-grid-community/core';
 import { AgGridReact } from '@ag-grid-community/react';
@@ -15,6 +15,7 @@ import { rowData, SellBondInterest } from './rowData';
 import { agGridModules } from './agGridModules';
 import { renderReactRoot } from '../react-18-utils';
 import '@interopio/theme-demo-apps/dist/io.applications.css';
+import { useIOConnect } from '@interopio/react-hooks';
 
 const renderWeakMap: WeakMap<HTMLElement, Root> = new WeakMap();
 
@@ -27,7 +28,6 @@ export const AxesBlotter = () => {
       columnDefs,
       rowData,
       sideBar: true,
-
       suppressMenuHide: true,
       enableRangeSelection: true,
       enableCharts: true,
@@ -121,6 +121,49 @@ export const AxesBlotter = () => {
   );
 
   const adaptableApiRef = React.useRef<AdaptableApi>();
+  const ISIN_CONTEXT = 'ISIN_CONTEXT';
+
+  useEffect(() => {
+    async function setMyWorkspaceId() {
+      const inWsp = await (window as any).io.workspaces?.inWorkspace();
+      if (!inWsp) {
+        return;
+      }
+
+      const myWorkspace = await (window as any).io.workspaces?.getMyWorkspace();
+      await (window as any).io.windows.my().updateContext({
+        workspaceId: myWorkspace?.id,
+      });
+    }
+
+    setMyWorkspaceId();
+  }, []);
+
+  useIOConnect(async (io) => {
+    const workspaceId = (await io.windows.my().getContext()).workspaceId;
+
+    const workspace =
+      (await io.workspaces?.getAllWorkspaces())?.find(
+        ({ id }) => id === workspaceId,
+      ) || (await io.workspaces?.getMyWorkspace());
+    if (!workspace) return;
+
+    return workspace.onContextUpdated((context: any) => {
+      const adaptableApi = adaptableApiRef.current;
+
+      if (ISIN_CONTEXT in context && adaptableApi) {
+        const isinValue = context[ISIN_CONTEXT].id?.ISIN;
+        const isinFilter: ColumnFilter = {
+          ColumnId: 'ISIN',
+          Predicate: {
+            PredicateId: 'Is',
+            Inputs: [isinValue],
+          },
+        };
+        adaptableApi.filterApi.setColumnFilters([isinFilter]);
+      }
+    });
+  });
 
   return (
     <div
